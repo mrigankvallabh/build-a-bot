203aee78e77aecb83226748c9720a6d367ec8f30
Mrigank Ballabh Singh <mrigank.ballabh@hk.yusen-logistics.com>
2020-12-15 09:56:56 +0800

    Initial Commit: Mod 2 - Basic App scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item-class">
+            <img src="./assets/build-a-bot-logo.png" alt="Bot Logo" class="logo">
+            Build a Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+main {
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: #fff;
+  width: 64rem;
+  min-height: 20rem;
 }
 </style>
